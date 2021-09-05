3df41b65b6d065dcb98681fda6909715b9439f8a
Jeb Nicholson <26765018+jeb5@users.noreply.github.com>
2021-09-05 18:04:10 +1200

    Added tab class to keep track of tab changes

--- sidebar/sidebarScript.js
+++ sidebar/sidebarScript.js
@@ -1,60 +1,117 @@
-const tabsDiv = document.getElementById("tabsDiv");
-
-browser.tabs.onActivated.addListener(tabChange);
-browser.tabs.onAttached.addListener(tabChange);
-browser.tabs.onCreated.addListener(tabChange);
-browser.tabs.onDetached.addListener(tabChange);
-browser.tabs.onHighlighted.addListener(tabChange);
-browser.tabs.onMoved.addListener(tabChange);
-browser.tabs.onRemoved.addListener(tabChange);
-browser.tabs.onReplaced.addListener(tabChange);
-browser.tabs.onUpdated.addListener(tabChange);
-
-tabChange();
-async function tabChange(...stuff) {
-	console.log("TABS CHANGED");
-	const tabs = await browser.tabs.query({ windowId: browser.windows.WINDOW_ID_CURRENT });
-	console.log(tabs);
-	while (tabsDiv.hasChildNodes()) {
-		tabsDiv.removeChild(tabsDiv.lastChild);
+class Tab {
+	constructor(rawTab) {
+		this.updated(rawTab);
+		this.id = rawTab.id;
+		const { tabEl, titleEl, faviconEl } = this.createTabEl();
+		[this.tabEl, this.titleEl, this.faviconEl] = [tabEl, titleEl, faviconEl];
+		this.updateDetails();
+		this.setActive(this.active);
+		tabsDiv.insertBefore(tabEl, tabsDiv.children[rawTab.index]);
 	}
-	for (const tab of tabs) {
-		tabsDiv.appendChild(createTabEl(tab));
+	get index() {
+		return [...tabsDiv.children].indexOf(this.tabEl);
+	}
+	updated(changeInfo) {
+		const newInfo = { ...this, ...changeInfo };
+		this.title = newInfo.title;
+		this.favIconUrl = newInfo.favIconUrl;
+		this.active = newInfo.active;
+	}
+	moved(toIndex) {
+		tabsDiv.insertBefore(this.tabEl, tabsDiv.children[toIndex]);
+	}
+	removeTab() {
+		tabsDiv.removeChild(this.tabEl);
+	}
+	updateDetails() {
+		this.titleEl.innerText = this.title;
+		this.faviconEl.src = this.favIconUrl;
+	}
+	setActive(active) {
+		this.active = active;
+		if (this.active) return this.tabEl.classList.add("activeTab");
+		this.tabEl.classList.remove("activeTab");
+	}
+	createTabEl() {
+		const tabEl = document.createElement("div");
+		const titleEl = document.createElement("div");
+		const faviconEl = document.createElement("img");
+		const tabCloseBtn = document.createElement("img");
+		tabEl.appendChild(faviconEl);
+		tabEl.appendChild(titleEl);
+		tabEl.appendChild(tabCloseBtn);
+
+		tabEl.addEventListener("click", async () => {
+			await browser.tabs.highlight({
+				windowId: WIN_ID,
+				populate: false,
+				tabs: [this.index],
+			});
+		});
+		tabEl.classList.add("tab");
+
+		titleEl.classList.add("tabText");
+
+		tabCloseBtn.classList.add("tabCloseBtn");
+		tabCloseBtn.src = browser.runtime.getURL("assets/close.svg");
+		tabCloseBtn.addEventListener("click", async e => {
+			e.stopPropagation();
+			await browser.tabs.remove(this.id);
+		});
+
+		faviconEl.classList.add("tabIcon");
+
+		return { tabEl, titleEl, faviconEl };
 	}
 }
+const tabsDiv = document.getElementById("tabsDiv");
+let currentTabs = {}; //All tabs in the current window, indexed by id
+let WIN_ID = null;
 
-function createTabEl(tabInfo) {
-	let tabEl = document.createElement("div");
-	let tabText = document.createElement("div");
-	let tabIcon = document.createElement("img");
-	let tabCloseBtn = document.createElement("img");
-	tabEl.appendChild(tabIcon);
-	tabEl.appendChild(tabText);
-	tabEl.appendChild(tabCloseBtn);
+function tabCreated(rawTab) {
+	currentTabs[rawTab.id] = new Tab(rawTab);
+}
+function tabRemoved(tabId) {
+	currentTabs[tabId].removeTab();
+	delete currentTabs[tabId];
+}
+function tabChanged(tabId, changeInfo) {
+	currentTabs[tabId].updated(changeInfo);
+	currentTabs[tabId].updateDetails();
+}
+function tabMoved(tabId, toIndex) {
+	currentTabs[tabId].moved(toIndex);
+}
 
-	tabEl.classList.add("tab");
-	if (tabInfo.active) {
-		tabEl.classList.add("activeTab");
-	}
-	tabEl.addEventListener("click", async () => {
-		await browser.tabs.highlight({ populate: false, tabs: [tabInfo.index] });
+setup();
+async function setup() {
+	WIN_ID = (await browser.windows.getCurrent()).id; //WINDOW_ID_CURRENT returns wrong value for some reason
+	browser.tabs.onActivated.addListener(async ({ tabId, previousTabId, windowId }) => {
+		if (windowId == WIN_ID) {
+			currentTabs[tabId].setActive(true);
+			currentTabs[previousTabId]?.setActive(false);
+		}
 	});
-
-	tabText.innerText = tabInfo.title;
-	tabText.classList.add("tabText");
-
-	tabCloseBtn.classList.add("tabCloseBtn");
-	tabCloseBtn.src = browser.runtime.getURL("assets/close.svg");
-	tabCloseBtn.addEventListener("click", async () => {
-		await browser.tabs.remove(tabInfo.id);
+	browser.tabs.onAttached.addListener(async (tabId, { newWindowId }) => {
+		if (newWindowId === WIN_ID) tabCreated(await browser.tabs.get(tabId));
 	});
+	browser.tabs.onCreated.addListener(tab => {
+		if (tab.windowId === WIN_ID) tabCreated(tab);
+	});
+	browser.tabs.onDetached.addListener((tabId, { oldWindowId }) => {
+		if (oldWindowId === WIN_ID) tabRemoved(tabId);
+	});
+	// browser.tabs.onHighlighted.addListener(tabChange);
+	browser.tabs.onMoved.addListener((tabId, { windowId, toIndex }) => {
+		if (windowId === WIN_ID) tabMoved(tabId, toIndex);
+	});
+	browser.tabs.onRemoved.addListener((tabId, { windowId }) => {
+		if (windowId === WIN_ID) tabRemoved(tabId);
+	});
+	browser.tabs.onUpdated.addListener(tabChanged, { windowId: WIN_ID });
 
-	tabIcon.classList.add("tabIcon");
-	if (tabInfo.favIconUrl) {
-		tabIcon.src = tabInfo.favIconUrl;
-	} else {
-		tabIcon.classList.add("noIcon");
-	}
-
-	return tabEl;
+	const tabs = await browser.tabs.query({ windowId: WIN_ID });
+	const oldTabs = Object.values(currentTabs);
+	for (const tab of oldTabs) tabRemoved(tab);
+	for (const tab of tabs) tabCreated(tab);
 }
